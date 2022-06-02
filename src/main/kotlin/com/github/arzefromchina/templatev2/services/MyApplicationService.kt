77c334b325dcdaf1c0edd1520e332488ce4d98a8
GitHub Action <action@github.com>
2022-06-02 08:36:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/arzefromchina/templatev2/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/arzefromchina/templatev2/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.arzefromchina.templatev2.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.arzefromchina.templatev2.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/arzefromchina/templatev2/services/MyProjectService.kt
+++ src/main/kotlin/com/github/arzefromchina/templatev2/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.arzefromchina.templatev2.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.arzefromchina.templatev2.MyBundle
 
 class MyProjectService(project: Project) {
 
